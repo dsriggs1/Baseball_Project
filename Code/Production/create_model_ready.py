0dc344c75e2fe0d1c480405ca17dc3518f7ab582
dsriggs1 <dsriggs@ncsu.edu>
2022-12-22 02:02:48 -0500

    Creating some more columns in the get_lineup method dataframes, and minor naming changes.

--- Code/Production/create_model_ready.py
+++ Code/Production/create_model_ready.py
@@ -45,7 +45,7 @@ class Database:
             con.execute(f'DELETE FROM {table_name} WHERE {where_clause}')
 
 
-class Weather(Database):
+class WebScrape(Database):
     def __init__(self, uid, pwd, host, db):
         super().__init__(uid, pwd, host, db)
         self.headers = {
@@ -88,26 +88,26 @@ class Weather(Database):
         df["HUMIDITY_PARK_CT"] = df["HUMIDITY_PARK_CT"].str.replace("%", "")
         return df
 
-    def get_lineups(self, url, date):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    def get_lineups(self, date):
 
         """
             Scrape the lineup information for a given date from the baseballpress.com website.
 
             Parameters:
-            url (str): The url of the webpage to be scraped.
             date (str): The date of the game for which the lineup is being scraped.
 
             Returns:
             tuple: A tuple containing two pandas dataframes, one for the batters and one for the pitchers.
             """
-        if not isinstance(url, str):
-            raise TypeError("'Url' must be a string")
+        if not isinstance(date, str):
+            raise TypeError("'date' must be a string in the format 'YYYYMMDD'")
 
-        res = requests.get(url, headers=headers)
+        res = requests.get(f"https://www.baseballpress.com/lineups/{date}", headers=self.headers)
 
         soup = BeautifulSoup(res.text, 'html.parser')
+
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+        yyyymmdd = date_obj.strftime('%Y%m%d')
 
         player_long = soup.find_all('div', attrs={'class': 'player'})
         players = soup.find_all(class_='player-link')
@@ -125,12 +125,13 @@ class Weather(Database):
 
         AWAY_HOME = ["AWAY_TEAM_ID", "HOME_TEAM_ID"]
 
+        # batters = pd.DataFrame(batters, columns=['player'])
         batters['TEAM'] = [TEAM for TEAM in TEAMS for i in range(9)]
         batters['BAT_LINEUP_ID'] = batters['player_long'].str.extract(r'(\d)')
         batters['POSITION'] = batters['player_long'].str.extract(r'\b(CF|RF|LF|1B|2B|3B|SS|C|DH)\b')
         batters['BAT_HAND_CD'] = batters['player_long'].str.extract(r'\b(R|L|S)\b')
 
-        self.position_mapping = {
+        position_mapping = {
             'P': 1,
             'C': 2,
             '1B': 3,
@@ -144,17 +145,62 @@ class Weather(Database):
             0: 999
         }
 
-        batters.fillna(0, inplace=True)
-        # Create a new column called 'BAT_FLD_CD' that maps the positions to the corresponding numbers
-        batters['BAT_FLD_CD'] = [position_mapping[POSITION] for POSITION in batters['POSITION']]
+        mlb_teams = {
+            "Angels": "ANA",
+            "Astros": "HOU",
+            "Athletics": "OAK",
+            "Blue Jays": "TOR",
+            "Braves": "ATL",
+            "Brewers": "MIL",
+            "Cardinals": "SLN",
+            "Cubs": "CHI",
+            "Diamondbacks": "ARI",
+            "Dodgers": "LAN",
+            "Giants": "SFN",
+            "Guardians": "CLE",
+            "Mariners": "SEA",
+            "Marlins": "MIA",
+            "Mets": "NYN",
+            "Nationals": "WAS",
+            "Orioles": "BAL",
+            "Padres": "SDN",
+            "Phillies": "PHI",
+            "Pirates": "PIT",
+            "Rangers": "TEX",
+            "Rays": "TBA",
+            "Red Sox": "BOS",
+            "Reds": "CIN",
+            "Rockies": "COL",
+            "Royals": "KCA",
+            "Tigers": "DET",
+            "Twins": "MIN",
+            "White Sox": "CHA",
+            "Yankees": "NYA",
+        }
 
-        AWAY_HOME = [item for sublist in [AWAY_HOME] * (len(batters) // len(AWAY_HOME) + 1) for item in sublist]
-        batters['AWAY_HOME_ID'] = [AWAY_HOME[i // 9] for i in range(len(batters))]
-        AWAY_HOME = ["AWAY_TEAM_ID", "HOME_TEAM_ID"]
-        AWAY_HOME = [item for sublist in [AWAY_HOME] * (len(pitchers) // len(AWAY_HOME) + 1) for item in sublist]
-        pitchers['AWAY_HOME_ID'] = [AWAY_HOME[i // 1] for i in range(len(pitchers))]
+        batters.fillna(0, inplace=True)
+
+        batters['BAT_FLD_CD'] = [position_mapping[POSITION] for POSITION in batters['POSITION']]
 
         batters.rename(columns={'players': 'BAT_ID'}, inplace=True)
         pitchers.rename(columns={'players': 'PIT_ID'}, inplace=True)
+
+        AWAY_TEAM_ID = [team for team in TEAMS[::2]]
+        HOME_TEAM_ID = [team for team in TEAMS if team not in AWAY_TEAM_ID]
+
+        batters['AWAY_TEAM_ID'] = [team for team in AWAY_TEAM_ID for i in range(18)]
+        batters['HOME_TEAM_ID'] = [team for team in HOME_TEAM_ID for i in range(18)]
+
+        pitchers['AWAY_TEAM_ID'] = [team for team in AWAY_TEAM_ID for i in range(2)]
+        pitchers['HOME_TEAM_ID'] = [team for team in HOME_TEAM_ID for i in range(2)]
+
+        batters['GAME_ID'] = [mlb_teams[team] for team in batters['HOME_TEAM_ID']]
+        batters['GAME_ID'] = batters['GAME_ID'] + yyyymmdd
+
+        pitchers['GAME_ID'] = [mlb_teams[team] for team in pitchers['HOME_TEAM_ID']]
+        pitchers['GAME_ID'] = pitchers['GAME_ID'] + yyyymmdd
+
+        batters.drop(columns=['player_long'], axis=1, inplace=True)
+        pitchers.drop(columns=['player_long'], axis=1, inplace=True)
 
         return batters, pitchers
