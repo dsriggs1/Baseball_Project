757b7fc8c1f8c0242f6d0b29aebf04b54e11a38d
dsriggs1 <dsriggs@ncsu.edu>
2023-01-01 19:08:53 -0500

    Creating a skeleton outline for an object oriented modeling python module.

---
+++ Code/OOM/Model.py
@@ -0,0 +1,24 @@
+class Model:
+    def __init__(self):
+        # Initialize the class
+        pass
+
+    def plot(self):
+        # Method for plotting
+        pass
+
+    def corr(self):
+        # Method for calculating correlations
+        pass
+
+    def missing_val(self):
+        # Method for handling missing values
+        pass
+
+    def vif(self):
+        # Method for calculating variance inflation factor
+        pass
+
+    def check_var_tpe(self):
+        # Method for checking variable type
+        pass
