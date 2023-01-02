6f3736b51962b30d2b0c3555f430c77e1680cc99
dsriggs1 <dsriggs@ncsu.edu>
2023-01-02 11:15:44 -0500

    Adding template files for Logistic Regression and Multinomial Logistic Regression classes.

---
+++ Code/OOM/Classification.py
@@ -0,0 +1,28 @@
+class Classification:
+    def __init__(self):
+        # Initialize the class
+        pass
+
+    def conf_matrix(self):
+        # Method for creating a confusion matrix
+        pass
+
+    def f_score(self):
+        # Method for calculating the F-score
+        pass
+
+    def auc_roc(self):
+        # Method for calculating the AUC-ROC curve
+        pass
+
+    def log_loss(self):
+        # Method for calculating the log loss
+        pass
+
+    def calibration(self):
+        # Method for creating a calibration plot
+        pass
+
+    def SomersD(self):
+        # Method for creating a calibration plot
+        pass
