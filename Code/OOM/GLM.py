e3ac7636f0857ceda87ff384ec1eb413187d0ee7
dsriggs1 <dsriggs@ncsu.edu>
2023-01-02 20:12:21 -0500

    Adding new class and method templates.

---
+++ Code/OOM/GLM.py
@@ -0,0 +1,8 @@
+# Import the Regression class
+from Regression import Regression
+
+class GLM(Regression):
+    def __init__(self):
+        super().__init__()
+        # Initialize the class
+        pass
---
+++ Code/OOM/OLS.py
@@ -0,0 +1,26 @@
+# Import the Regression class
+from Regression import Regression
+
+class OLS(Regression):
+    def __init__(self):
+        super().__init__()
+        # Initialize the class
+        pass
+    def normality(self):
+        # Method for testing normality
+        pass
+
+    def homoscedasticity(self):
+        # Method for testing homoscedasticity
+        pass
+    def multicollinearity(self):
+        # Method for testing multicollinearity
+        pass
+
+    def autocorrelation(self):
+        # Method for testing autocorrelation
+        pass
+
+    def heteroscedasticity(self):
+        # Method for testing heteroscedasticity
+        pass
