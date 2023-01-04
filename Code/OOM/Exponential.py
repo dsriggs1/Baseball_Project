1fba7553ecd73fce8eeffc2b8f065146b696fa8b
dsriggs1 <dsriggs@ncsu.edu>
2023-01-04 17:47:54 -0500

    Adding skeleton classes for exponential family GLMs.

---
+++ Code/OOM/Exponential.py
@@ -0,0 +1,8 @@
+from GLM import GLM
+
+
+class Exponential(GLM):
+    def __init__(self):
+        super().__init__()
+        # Initialize the class
+        pass
---
+++ Code/OOM/Poisson.py
@@ -0,0 +1,8 @@
+from GLM import GLM
+
+
+class Poisson(GLM):
+    def __init__(self):
+        super().__init__()
+        # Initialize the class
+        pass
