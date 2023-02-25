4a9d6da8591ed6373f3525d70a8c1f5dd323c223
Sean <dsriggs1@gmail.com>
2023-02-25 17:48:14 -0500

    Adding fit method for logistic regression.

--- Code/OOM/LogisticRegression.py
+++ Code/OOM/LogisticRegression.py
@@ -1,17 +1,26 @@
 # Import the Classification class
-from Classification import Classification
-from GLM import GLM
+from sklearn.linear_model import LogisticRegression as lr
+import polars as pl
 
-
-class LogisticRegression(GLM, Classification):
+class LogisticRegression():
     def __init__(self):
         super().__init__()
-        # Initialize the class
-        pass
+        self.model = lr()
+        self.coef_ = None
 
-    def fit(self):
-        # Method for fitting the model
-        pass
+    def fit(self, x, y):
+
+        if isinstance(x, pl.DataFrame):
+            x = x.to_numpy().reshape(-1, 1)
+        if isinstance(y, pl.DataFrame):
+            y = y.to_numpy().reshape(-1, 1)
+            y= y.ravel()
+
+        self.model.fit(x, y)
+        self.coef_ = self.model.coef_
+
+        return self.model
+
 
     def predict(self):
         # Method for making predictions
