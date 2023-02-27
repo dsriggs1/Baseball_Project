e15418b26be2684465b61b17c7da70d0d9fc7890
Sean <dsriggs1@gmail.com>
2023-02-26 20:17:23 -0500

    Adding a predcit method into the logistic regression class.

--- Code/OOM/LogisticRegression.py
+++ Code/OOM/LogisticRegression.py
@@ -3,13 +3,17 @@ from sklearn.linear_model import LogisticRegression as lr
 import polars as pl
 
 class LogisticRegression():
-    def __init__(self):
-        super().__init__()
-        self.model = lr()
+    """Custom implementation of logistic regression using scikit-learn's
+    LogisticRegression class designed to be able to handle polars dataframes as input
+    """
+
+    def __init__(self, C=1.0, penalty='l2', solver='lbfgs', max_iter=100):
+        """Initialize logistic regression model with hyperparameters"""
+        self.model = LR(C=C, penalty=penalty, solver=solver, max_iter=max_iter)
         self.coef_ = None
 
     def fit(self, x, y):
-
+        """Fit logistic regression model to the input data"""
         if isinstance(x, pl.DataFrame):
             x = x.to_numpy().reshape(-1, 1)
         if isinstance(y, pl.DataFrame):
@@ -21,10 +25,12 @@ class LogisticRegression():
 
         return self.model
 
+    def predict(self, x):
+        """Predict output labels for the input data"""
+        if isinstance(x, pl.DataFrame):
+            x = x.to_numpy().reshape(-1, 1)
 
-    def predict(self):
-        # Method for making predictions
-        pass
+        return self.model.predict(x)
 
     def LASSO(self):
         # Method for LASSO regularization
