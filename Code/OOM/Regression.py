5913c017df62af574f9b99647b3b33e39eecddc9
Sean <dsriggs1@gmail.com>
2023-03-12 20:19:15 -0400

    Defining x and y variables in the base Regression class, and making sure that polars dataframe inputs get converted to numpy arrays.

--- Code/OOM/Regression.py
+++ Code/OOM/Regression.py
@@ -1,11 +1,22 @@
 import numpy as np
-
+import polars as pl
 class Regression:
     """
-    Base class for regression based models. Should not intended to be directly called.
+    Base class for regression based models. Not intended to be directly called.
     Intended for subclassing.
     """
-    def __init__(self):
+    def __init__(self, x: np.ndarray, y: np.ndarray):
+        if isinstance(x, pl.DataFrame):
+            x = x.to_numpy()
+
+        if isinstance(y, pl.DataFrame):
+            y = y.to_numpy()
+
+        if not isinstance(x, np.ndarray):
+            raise TypeError("Input data X must be a numpy.ndarray")
+
+        self.x = x
+        self.y = y
         self.coefficients = None
 
     def predict(self, X: np.ndarray) -> np.ndarray:
@@ -17,8 +28,12 @@ class Regression:
         Returns:
         numpy.ndarray: Predicted target variable of shape (n_samples,).
         """
+        if isinstance(X, pl.DataFrame):
+            X = X.to_numpy()
+
         if not isinstance(X, np.ndarray):
             raise TypeError("Input data X must be a numpy.ndarray")
+
         X = np.insert(X, 0, 1, axis=1)  # Add bias term
         return X.dot(self.coefficients)
 
