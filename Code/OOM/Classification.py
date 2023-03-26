7bca885a2923002f06c08c3a75b9a01135aec914
Sean <dsriggs1@gmail.com>
2023-03-26 12:11:17 -0400

    Adding variable handling for polars dataframe to the classification class, and adding a predict method.

--- Code/OOM/Classification.py
+++ Code/OOM/Classification.py
@@ -1,13 +1,14 @@
 from typing import Union
 import numpy as np
 import pandas as pd
+import polars as pl
 from sklearn.metrics import confusion_matrix, f1_score, roc_auc_score, log_loss
 from sklearn.calibration import calibration_curve
 import matplotlib.pyplot as plt
 
 
 class Classification:
-    def __init__(self, y_true: Union[list, np.ndarray], y_pred: Union[list, np.ndarray]):
+    def __init__(self, x: np.ndarray, y: np.ndarray):#, y_true: Union[list, np.ndarray], y_pred: Union[list, np.ndarray]):
         """
         Initialize the Classification class.
 
@@ -18,17 +19,49 @@ class Classification:
         Returns:
         None
         """
-        if not isinstance(y_true, (list, np.ndarray)) or not isinstance(y_pred, (list, np.ndarray)):
-            raise TypeError("y_true and y_pred must be a list or numpy array.")
 
-        if len(y_true) != len(y_pred):
-            raise ValueError("y_true and y_pred must have the same length.")
+        if isinstance(x, pl.DataFrame):
+            x = x.to_numpy().reshape(-1, 1)
 
-        self.y_true = y_true
-        self.y_pred = y_pred
+        if isinstance(y, pl.DataFrame):
+            y= y.to_numpy().reshape(-1, 1)
+            y= y.ravel()
 
-    def predict(self):
-        pass
+        #if not isinstance(y_true, (list, np.ndarray)) or not isinstance(y_pred, (list, np.ndarray)):
+            #raise TypeError("y_true and y_pred must be a list or numpy array.")
+
+        #if len(y_true) != len(y_pred):
+            #raise ValueError("y_true and y_pred must have the same length.")
+
+        #self.y_true = y_true
+        #self.y_pred = y_pred
+        self.x = x
+        self.y = y
+        self.coefficients = None
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict binary classification output based on a numpy array of independent variables and a vector of coefficients.
+
+        Parameters:
+        X (numpy array): Numpy array of independent variables.
+        coef (numpy array): Vector of coefficients between 0 and 1.
+
+        Returns:
+        numpy array: Binary classification output.
+        """
+        if isinstance(X, pl.DataFrame):
+            X = X.to_numpy()
+
+        if not isinstance(X, np.ndarray) or not isinstance(self.coefficients, np.ndarray):
+            raise TypeError("X and coef must be numpy arrays.")
+
+        if X.shape[1] != self.coefficients.shape[0]:
+            raise ValueError("X and coef must have compatible dimensions.")
+
+        y_pred = X.dot(self.coefficients)
+
+        return y_pred
 
     def conf_matrix(self) -> np.ndarray:
         """
